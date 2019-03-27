cb20e57e960c9b039a3034fd860bef0572607589
Alaedeen Eloueryemmi <alaedeen95@gmail.com>
2019-03-27 15:57:50 +0100

    fix dashboard

--- src/Components/administration/Admins.vue
+++ src/Components/administration/Admins.vue
@@ -10,21 +10,76 @@
               clearable
               v-model="search"></v-text-field>
       </v-flex>
-      <v-flex  xs12 style="margin: 2em; box-shadow: -1px 1px 5px grey;padding: 1em" v-for="(user, index) in users" :key="index">
-        <app-admin :user="user"></app-admin>
-      </v-flex>
+      <v-data-table
+        :headers="headers"
+        :items="users"
+        class="elevation-1"
+        dark
+      >
+        <template v-slot:items="props" >
+          <tr style="cursor: pointer" >
+            <td class="text-xs-center" >{{ props.item.name }}</td>
+          <td class="text-xs-center">{{ props.item.email }}</td>
+          <td class="text-xs-center">{{ props.item.joined.month }} {{ props.item.joined.day }}, {{ props.item.joined.year }}</td>
+          <td class="text-xs-center">{{ props.item.subscribers }}</td>
+          <td><v-btn color="error" @click="removeBtn(props.item.id,props.item.name)">remove admin</v-btn></td>
+          </tr>
+        </template>
+      </v-data-table>
+
+      <!-- remove admin dialog -->
+            <v-dialog
+                v-model="dialog1"
+                min-width="350"
+                max-width="400"
+              >
+                <v-card >
+                  <v-card-title class="headline">Remove {{name}} as an admin?</v-card-title>
+
+                  <v-card-text>
+                    Are you sure ?
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      color="green darken-1"
+                      flat="flat"
+                      @click="dialog1 = false"
+                    >
+                      no
+                    </v-btn>
+
+                    <v-btn
+                      color="red darken-1"
+                      flat="flat"
+                      @click="removeAdmin(id)"
+                    >
+                      yes
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            <!-- remove admin dialog end -->
   </div>
 </template>
 
 <script>
-import Admin from './Admin.vue'
 export default {
-  components: {
-    appAdmin: Admin
-  },
   data() {
     return {
+      headers: [
+        { text: 'User name',align: 'center',sortable: false,value: 'user name' },
+        { text: 'Email ',align: 'center',sortable: false, value: 'email' },
+        { text: 'Joined',align: 'center', sortable: false,value: 'joined' },
+        { text: 'N° subscribers',align: 'center',sortable: false, value: 'subscribers' },
+        { text: '',align: 'center',sortable: false, value: '' },
+      ],
       search: '',
+      dialog1: false,
+      name:'',
+      id: 0
     }
   },
 computed: {
@@ -43,6 +98,17 @@ watch: {
         this.search=''
       }
     },
+  },
+  methods: {
+    removeBtn(id,name){
+      this.id=id
+      this.name=name
+      this.dialog1 = true
+    },
+    removeAdmin(id){
+      this.dialog1 = false
+      this.$store.dispatch('user/removeAdmin',id)
+    }
   },
 
 }
--- src/Components/administration/Users.vue
+++ src/Components/administration/Users.vue
@@ -10,26 +10,117 @@
               clearable
               v-model="search"></v-text-field>
       </v-flex>
-      <v-flex  xs12 style="margin: 2em; box-shadow: -1px 1px 5px grey;padding: 1em" v-for="(user, index) in users" :key="index">
-        <app-user :user="user" :role="role"></app-user>
-      </v-flex>
+      <v-data-table
+        :headers="headers"
+        :items="users"
+        class="elevation-1"
+        dark
+      >
+        <template v-slot:items="props" >
+          <tr style="cursor: pointer" >
+            <td class="text-xs-center" >{{ props.item.name }}</td>
+          <td class="text-xs-center">{{ props.item.email }}</td>
+          <td class="text-xs-center">{{ props.item.joined.month }} {{ props.item.joined.day }}, {{ props.item.joined.year }}</td>
+          <td class="text-xs-center">{{ props.item.subscribers }}</td>
+          <td><v-btn color="error" @click="deleteBtn(props.item.id,props.item.name)">Delete</v-btn></td>
+          <td><v-btn color="warning" >bann</v-btn></td>
+          <td><v-btn color="success" @click="addAdminBtn(props.item.id,props.item.name)" v-if="current.roles.length==3">add admin</v-btn></td>
+          </tr>
+        </template>
+      </v-data-table>
+
+       <!-- delete dialog -->
+            <v-dialog
+                v-model="dialog1"
+                min-width="350"
+                max-width="400"
+              >
+                <v-card >
+                  <v-card-title class="headline">Delete {{name}} account?</v-card-title>
+
+                  <v-card-text>
+                    Are you sure ?
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      color="green darken-1"
+                      flat="flat"
+                      @click="dialog1 = false"
+                    >
+                      no
+                    </v-btn>
+
+                    <v-btn
+                      color="red darken-1"
+                      flat="flat"
+                      @click="deleteUser(id)"
+                    >
+                      yes
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            <!-- delete dialog end -->
+            <!-- add admin dialog -->
+                <v-dialog
+                    v-model="dialog2"
+                    min-width="350"
+                    max-width="400"
+                  >
+                    <v-card >
+                      <v-card-title class="headline">Add {{name}} as an admin?</v-card-title>
+
+                      <v-card-text>
+                        Are you sure ?
+                      </v-card-text>
+
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+
+                        <v-btn
+                          color="yellow darken-1"
+                          flat="flat"
+                          @click="dialog2 = false"
+                        >
+                          no
+                        </v-btn>
+
+                        <v-btn
+                          color="green darken-1"
+                          flat="flat"
+                          @click="addAdmin(id)"
+                        >
+                          yes
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                <!-- add admin dialog end -->
   </div>
 </template>
 
 <script>
-import User from './User.vue'
 export default {
-  props: {
-    role:{
-      type: Number
-    }
-  },
-  components: {
-    appUser: User
-  },
+
   data() {
     return {
+      headers: [
+        { text: 'User name',align: 'center',sortable: false,value: 'user name' },
+        { text: 'Email ',align: 'center',sortable: false, value: 'email' },
+        { text: 'Joined',align: 'center', sortable: false,value: 'joined' },
+        { text: 'N° subscribers',align: 'center',sortable: false, value: 'subscribers' },
+        { text: '',align: 'center',sortable: false, value: '' },
+        { text: '',align: 'center',sortable: false, value: '' },
+        { text: '',align: 'center',sortable: false, value: '' },
+      ],
       search: '',
+      dialog1: false,
+      dialog2: false,
+      name:'',
+      id: 0
     }
   },
 computed: {
@@ -40,6 +131,9 @@ computed: {
     return all.filter(one => {
       return one.name.toUpperCase().includes(this.search.toUpperCase())
     });
+  },
+  current(){
+        return this.$store.state.user.current
   }
 },
 watch: {
@@ -48,6 +142,27 @@ watch: {
         this.search=''
       }
     },
+  },
+  methods: {
+    deleteBtn(id,name){
+      this.id=id
+      this.name=name
+      this.dialog1 = true
+    },
+    addAdminBtn(id,name){
+      this.id=id
+      this.name=name
+      this.dialog2 = true
+    },
+    deleteUser(id){
+      this.dialog1 = false
+      //alert(id)
+      this.$store.dispatch('user/deleteUser',id)
+    },
+    addAdmin(id){
+      this.dialog2 = false
+      this.$store.dispatch('user/addAdmin',id)
+    }
   },
 
 }

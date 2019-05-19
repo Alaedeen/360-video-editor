13c0678e59610e9a2f1f284707270e1fad04f599
Alaedeen Eloueryemmi <alaedeen95@gmail.com>
2019-05-19 15:16:56 +0100

    implement send a sharing request interface

--- src/Components/projects/projectTile.vue
+++ src/Components/projects/projectTile.vue
@@ -19,6 +19,7 @@
                 small
                 right
                 top
+                @click="shareProject"
               >
                 <v-icon>share</v-icon>
               </v-btn>
@@ -28,16 +29,56 @@
 
         </v-card>
 
+          <v-dialog
+        v-model="dialog"
+        max-width="350"
+      >
+        <v-card>
+          <v-card-title class="headline">Make a sharing request!</v-card-title>
+
+          <v-card-text>
+            Are you sure!! <br> By pressing yes you are going to send a sharing request, this video <b> "{{project.title}}"</b> won't be public without the administration aproval.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              color="green darken-1"
+              flat="flat"
+              @click="dialog = false"
+            >
+              YES
+            </v-btn>
+
+            <v-btn
+              color="yellow darken-1"
+              flat="flat"
+              @click="dialog = false"
+            >
+              NO
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 </div>
 </template>
 
 <script>
 export default {
   props: ['project'],
+  data() {
+    return {
+      dialog: false,
+    }
+  },
   methods: {
     edit(id){
       var url = '/edit/'+id
       this.$router.push({path:url})
+    },
+    shareProject(){
+      this.dialog=true
     }
   },
 
